a4f542c0e8d087a61771c888207626ece82b2c31
spade5 <spade5@spade5.name>
2018-04-09 18:55:37 +0800

    写点样式

--- src/js/home.jsx
+++ src/js/home.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 export default class Home extends React.Component {
-    render(){
-      return (
-        <div className='page'>
-        	<a href='http://baidu.com'>baidu</a>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+  	this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
